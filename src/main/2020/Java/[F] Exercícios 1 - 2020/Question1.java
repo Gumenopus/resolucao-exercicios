f8d89dd1eb1bac10da008fd6eb1bca470d2415b6
Gustabox <gustavo.dickmann123@gmail.com>
2020-10-15 12:17:43 -0300

    - Comentários removidos

--- src/main/2020/Java/[F] Exercícios 1 - 2020/Question1.java
+++ src/main/2020/Java/[F] Exercícios 1 - 2020/Question1.java
@@ -17,44 +17,10 @@ class Question1 {
             int valorPorHora = scanner.nextInt();
 
             if (quantidadeDeHorasTrabalhadas > 160) {
-
-                // int horasExtras = quantidadeDeHorasTrabalhadas - 160;
-                // int cinquentaPorcentoDoSalario = valorPorHora / 2 + valorPorHora;
-
-                // int valorFinalHoraExtra =  horasExtras * cinquentaPorcentoDoSalario;
-                // int valorFinal = 160 * valorPorHora + valorFinalHoraExtra;
-
                 System.out.println("Salário total: " + ( (quantidadeDeHorasTrabalhadas - 160) * (valorPorHora / 2 + valorPorHora) + (160 * valorPorHora)) );
             } else {
                 System.out.println("Salário total: " + quantidadeDeHorasTrabalhadas * valorPorHora);
             }
-
-            // System.out.println("Salário do funcionário sem horas extras: R$" +
-            // calculaHoraPadrao(valorPorHora));
-
-            // System.out.println("Horas extras do funcionário: R$" +
-            // calculaHoraExtra(quantidadeDeHorasTrabalhadas, valorPorHora));
-
-            // System.out.println("Salário total do funcionário: R$"+
-            // calculaSalarioTotal(quantidadeDeHorasTrabalhadas, valorPorHora));
         }
-    }
-
-    private static int calculaHoraExtra(int quantidadeDeHorasTrabalhadas, int valorPorHora) {
-        int horasExtrasTrabalhadas = quantidadeDeHorasTrabalhadas - quantidadeDeHorasMensais;
-        int cinquentaPorcentoDoSalario = valorPorHora / 2;
-
-        int valorExtra = valorPorHora + cinquentaPorcentoDoSalario;
-        int totalDoValorExtra = valorExtra * horasExtrasTrabalhadas;
-
-        return totalDoValorExtra;
-    }
-
-    private static int calculaHoraPadrao(int valorPorHora) {
-        return quantidadeDeHorasMensais * valorPorHora;
-    }
-
-    private static int calculaSalarioTotal(int quantidadeDeHorasTrabalhadas, int valorPorHora) {
-        return calculaHoraPadrao(valorPorHora) + calculaHoraExtra(quantidadeDeHorasTrabalhadas, valorPorHora);
     }
 }
