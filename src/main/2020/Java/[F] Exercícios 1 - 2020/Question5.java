c9b138de32bee97dc0b29bfe383287f20a5fa1eb
Gustabox <gustavo.dickmann123@gmail.com>
2020-10-14 22:01:35 -0300

    - Question 5 resolvida

--- src/main/2020/Java/[F] Exercícios 1 - 2020/Question5.java
+++ src/main/2020/Java/[F] Exercícios 1 - 2020/Question5.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 class Question5 {
+
     public static void main(String[] args) {
-        //TODO...
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Informe o caractere de gênero: ");
+            String palavra = scanner.nextLine();
+
+            if (palavraEValida(palavra.toLowerCase())) {
+                System.out.println(retornaCaractereAbreviado(palavra.toUpperCase()));
+            } else {
+                System.out.println("Entrada incorreta.");
+            }
+        }
+    }
+
+    private static String retornaCaractereAbreviado(String palavra) {
+        if (palavra.equals("M")) {
+            return "Masculino.";
+        }
+        if (palavra.equals("F")) {
+            return "Feminino.";
+        }
+        if (palavra.equals("I")) {
+            return "Não informado.";
+        }
+        return null;
+    }
+
+    private static boolean palavraEValida(String palavra) {
+        return !palavra.isEmpty() && palavra.equals("m") || palavra.equals("f") || palavra.equals("i");
     }
 }
